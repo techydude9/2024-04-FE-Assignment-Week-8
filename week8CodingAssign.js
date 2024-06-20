e2f89e5ec72ea444b88bf816668c3ebb19188077
techydude9 <bob.ruzga@gmail.com>
2024-06-20 15:05:19 -0400

    First push for baseline code

---
+++ week8CodingAssign.js
@@ -0,0 +1,13 @@
+/* Week 8
+    javaScript file
+    Author: Bob Ruzga
+    Date: June 20, 2024
+*/
+
+/* Week 8 Coding Step Requirements:
+    Create a menu app as seen in this week’s video. What you create is up to you as long as it meets the following requirements:
+        Use at least one array.
+        Use at least two classes.
+        Your menu should have the options to create, view, and delete elements.
+*/
+
